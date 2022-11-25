ba63d1508f6ce3791482286ab8ffa5f03e05cf6c
Elijah Kim <elijah10kim@gmail.com>
2022-11-25 12:12:51 -0500

    completed js exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    const arrayChars = string.split("").reverse();
+    for (const char of arrayChars) {
+        reversedString += char;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
