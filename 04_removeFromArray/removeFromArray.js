592acd2422dafdb683ffcc0bee75310ee91ac5b4
Elijah Kim <elijah10kim@gmail.com>
2022-11-25 12:36:16 -0500

    completed js exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...moreArgs) {
+    outer: for (const arg of moreArgs) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === arg) {
+                array.splice(i,1);
+                continue outer;
+            }
+        }
+    } return array;
 };
 
 // Do not edit below this line
