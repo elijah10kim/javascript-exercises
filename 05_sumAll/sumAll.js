4814951ad1d8a785b163a4b98c0be925536f81b4
Elijah Kim <elijah10kim@gmail.com>
2022-11-25 12:49:32 -0500

    completed js exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let largeNum;
+    let smallNum;
+    let sum = 0;
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR"
+    } else if (num1 < 0 || num2 < 0){
+        return "ERROR"
+    }
+    if (num1 < num2) {
+        largeNum = num2;
+        smallNum = num1;
+    } else {
+        largeNum = num1;
+        smallNum = num2;
+    }
+    for (let i = smallNum; i <= largeNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
