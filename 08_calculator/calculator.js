6d7fe1639d09780185dcd9361e529ee08cdbb44c
Elijah <elijah10kim@gmail.com>
2023-01-11 18:50:50 -0500

    completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arrayNumbers) {
+	return arrayNumbers.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arrayNumbers) {
+  if (arrayNumbers.length === 0){
+    return arrayNumbers.length;
+  }
+  return arrayNumbers.reduce((product, current) => product * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+   return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0){
+    return 1;
+  }
+  return n * factorial(n-1);
 };
 
 // Do not edit below this line
