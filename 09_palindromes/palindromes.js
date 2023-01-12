6aea40042f0d966275a933319b0b9549ac2a62af
Elijah <elijah10kim@gmail.com>
2023-01-12 15:26:31 -0500

    completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    let newString = string.toLowerCase().replace(/[^a-z]/g, '');
+    let reverseString = [...newString].reverse().join('');
+    return newString === reverseString;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
