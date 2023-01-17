50ae8f7abbb182de27ad84b21cd9d42bc0593e19
Elijah <elijah10kim@gmail.com>
2023-01-16 20:52:23 -0500

    completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map((book) => book.title);
 };
+
+
+/* Or this solution with for loop
+const getTheTitles = function(array) {
+    let titles = [];
+    for (let i = 0; i < array.length; i++) {
+      titles.push(array[i].title)
+    }
+    return titles;
+};
+*/
 
 // Do not edit below this line
 module.exports = getTheTitles;
