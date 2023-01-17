afecf87ffc04439ac1309b4310859ca114175b75
Elijah <elijah10kim@gmail.com>
2023-01-16 20:32:05 -0500

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    }
+    if (n == 0) {
+        return n;
+    }
+    let num1 = 0;
+    let num2 = 1;
+    for (let i = 1; i < n; i++) {
+        let pivot = num2;
+        num2 += num1;
+        num1 = pivot;
+    }
+    return num2;
 };
 
 // Do not edit below this line
