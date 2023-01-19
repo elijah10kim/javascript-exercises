8019519d6e8664336bad99fd899f6c2b9dde14fe
Elijah <elijah10kim@gmail.com>
2023-01-18 19:25:22 -0500

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = people.map(getAge);
+    let indexOldest = ages.indexOf(Math.max(...ages));
+    return people[indexOldest];
 };
+
+function getAge(person) {
+  let deathYear;
+  if (typeof person.yearOfDeath === "undefined") {
+    deathYear = new Date().getFullYear();
+  } else {
+    deathYear = person.yearOfDeath;
+  }
+  let birthYear = person.yearOfBirth;
+  return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
